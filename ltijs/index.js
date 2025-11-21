5bee5253437fe75a5bdc25eb6b8a46736bdf6ade
Klezya <benjamin.nunez.beat@gmail.com>
2025-11-21 09:14:43 -0300

    hotfix deploy, canvas iss fixed

--- ltijs/index.js
+++ ltijs/index.js
@@ -76,12 +76,12 @@ const setup = async () => {
     authConfig: { method: 'JWK_SET', key: 'https://edurundev.milaulas.com/mod/lti/certs.php' }
   })
    await lti.registerPlatform({
-    url: 'https://172.19.202.187',
+    url: 'https://201.187.236.176',
     name: 'canvas',
     clientId: '10000000000007',
-    authenticationEndpoint: 'https://172.19.202.187/api/lti/authorize_redirect',
-    accesstokenEndpoint: 'https://172.19.202.187/login/oauth2/token',
-    authConfig: { method: 'JWK_SET', key: 'https://172.19.202.187/api/lti/security/jwks' }
+    authenticationEndpoint: 'https://201.187.236.176/api/lti/authorize_redirect',
+    accesstokenEndpoint: 'https://201.187.236.176/login/oauth2/token',
+    authConfig: { method: 'JWK_SET', key: 'https://201.187.236.176/api/lti/security/jwks' }
   })
 }
 
